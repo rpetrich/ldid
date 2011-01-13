38d76981e2c2bd2aecf2190146548b3a1a478fd4
Jay Freeman (saurik) <saurik@saurik.com>
2011-01-13 05:23:33 -0800

    Support compilation on Mac OS X 10.4.

--- make.sh
+++ make.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-g++ -arch ppc -arch i386 -arch x86_64 -o ldid ldid.cpp -I. -x c lookup2.c sha1.c
+
+flags=()
+
+sdk=/Developer/SDKs/MacOSX10.4u.sdk
+if [[ -e $sdk ]]; then
+    flags+=(-mmacosx-version-min=10.4 -isysroot "$sdk")
+fi
+
+g++ -arch ppc -arch i386 -arch x86_64 "${flags[@]}" -o ldid ldid.cpp -I. -x c lookup2.c sha1.c
