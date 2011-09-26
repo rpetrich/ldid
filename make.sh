da59192ca38d826b8c020da9d1287f566203749b
Ryan Petrich <rpetrich@gmail.com>
2011-09-26 00:43:18 -0600

    Update make.sh to build out of box on Snow Leopard; include minimal headers

--- make.sh
+++ make.sh
@@ -4,9 +4,9 @@ set -e -x
 
 flags=()
 
-sdk=/Developer/SDKs/MacOSX10.4u.sdk
+sdk=/Developer/SDKs/MacOSX10.5.sdk
 if [[ -e $sdk ]]; then
     flags+=(-mmacosx-version-min=10.4 -isysroot "$sdk")
 fi
 
-g++ -arch ppc -arch i386 -arch x86_64 "${flags[@]}" -o ldid ldid.cpp -I. -x c lookup2.c sha1.c
+g++ -arch i386 -arch x86_64 "${flags[@]}" -o ldid ldid.cpp -I. -x c lookup2.c sha1.c
