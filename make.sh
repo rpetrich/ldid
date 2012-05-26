2acc33f0aa14fe129a99074d6882320f236d762a
Cody Brocious <cody.brocious@gmail.com>
2012-05-26 00:28:19 -0400

    Modified make.sh to work under Linux.

--- make.sh
+++ make.sh
@@ -2,11 +2,15 @@
 
 set -e -x
 
-flags=()
+if [[ `uname` == 'Darwin' ]]; then
+	flags=()
 
-sdk=/Developer/SDKs/MacOSX10.5.sdk
-if [[ -e $sdk ]]; then
-    flags+=(-mmacosx-version-min=10.4 -isysroot "$sdk")
+	sdk=/Developer/SDKs/MacOSX10.5.sdk
+	if [[ -e $sdk ]]; then
+	    flags+=(-mmacosx-version-min=10.4 -isysroot "$sdk")
+	fi
+
+	g++ -arch i386 -arch x86_64 "${flags[@]}" -o ldid ldid.cpp -I. -x c lookup2.c sha1.c
+elif [[ `uname` == 'Linux' ]]; then
+	g++ -pipe -o ldid ldid.cpp -I. -x c lookup2.c sha1.c
 fi
-
-g++ -arch i386 -arch x86_64 "${flags[@]}" -o ldid ldid.cpp -I. -x c lookup2.c sha1.c
