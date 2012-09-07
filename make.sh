bf29b0fecdb4e450107e1b304221b08339906aa5
Jay Freeman (saurik) <saurik@saurik.com>
2012-09-06 19:12:27 -0700

    Attept to autodetect support gcc architectures.

--- make.sh
+++ make.sh
@@ -9,4 +9,11 @@ if [[ -e $sdk ]]; then
     flags+=(-mmacosx-version-min=10.4 -isysroot "$sdk")
 fi
 
-g++ -arch ppc -arch i386 -arch x86_64 "${flags[@]}" -o ldid ldid.cpp -I. -x c lookup2.c sha1.c
+for arch in i386 x86_64 ppc armv6; do
+    if g++ -arch "${arch}" --version &>/dev/null; then
+        flags+=(-arch "${arch}")
+    fi
+done
+
+set -x
+g++ "${flags[@]}" -o ldid ldid.cpp -I. -x c lookup2.c sha1.c
