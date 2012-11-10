0a93f3e5908382e10bab708551775d80bfdfbdcd
polyvertex <polyvertex@gmail.com>
2012-11-10 13:55:10 +0100

    Cygwin support.

--- make.sh
+++ make.sh
@@ -2,11 +2,10 @@
 
 set -e -x
 
-flags=()
-
-sdk=/Developer/SDKs/MacOSX10.5.sdk
-if [[ -e $sdk ]]; then
-    flags+=(-mmacosx-version-min=10.4 -isysroot "$sdk")
+if [ -z "$(uname -a | grep -i cygwin)" ]; then
+  sdk=/Developer/SDKs/MacOSX10.5.sdk
+  flags=(-arch i386 -arch x86_64)
+  [ -e $sdk ] && flags+=(-mmacosx-version-min=10.4 -isysroot "$sdk")
 fi
 
-g++ -arch i386 -arch x86_64 "${flags[@]}" -o ldid ldid.cpp -I. -x c lookup2.c sha1.c
+g++ "${flags[@]}" -o ldid ldid.cpp -I. -x c lookup2.c sha1.c
