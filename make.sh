710d1b7fb96ab83cc3ccccdffdaff7443e83963c
polyvertex <polyvertex@gmail.com>
2012-11-13 21:32:53 +0100

    Typo...

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 
 set -e -x
 
-if [ -z "$(uname -a | grep -i cygwin)" ]; then
+if [ -z "$(uname -s | grep -i cygwin)" ]; then
   sdk=/Developer/SDKs/MacOSX10.5.sdk
   flags=(-arch i386 -arch x86_64)
   [ -e $sdk ] && flags+=(-mmacosx-version-min=10.4 -isysroot "$sdk")
